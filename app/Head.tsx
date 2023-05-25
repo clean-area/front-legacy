d6a5b0fb9c9475ee6894dfc6abaa7a98c1d8fff3
LeeSangCheol <leesangcheol@LeeSangCheols-MacBook-Air.local>
2023-05-26 00:52:10 +0900

    feat : 헤드 안 서브 메뉴 바 추가

--- app/Head.tsx
+++ app/Head.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import DropDown from "./DropDown";
+import SubMenuBar from "./SubMenuBar";
 
 const targets = [
   "대상선택",
@@ -22,18 +23,21 @@ export default function Head() {
   const [age, setAge] = useState("연령선택");
 
   return (
-    <div className="flex items-center justify-between w-full px-[25px] h-[32px]">
-      <div className="space-x-[12px]">
-        <DropDown list={targets} selected={target} setSelected={setTarget} />
-        <DropDown list={ages} selected={age} setSelected={setAge} />
+    <div className="flex flex-col space-y-[20px]">
+      <div className="flex items-center justify-between w-full px-[25px] h-[32px]">
+        <div className="space-x-[12px]">
+          <DropDown list={targets} selected={target} setSelected={setTarget} />
+          <DropDown list={ages} selected={age} setSelected={setAge} />
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          className="w-[20px] cursor-pointer"
+        >
+          <path d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748Z"></path>
+        </svg>
       </div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        className="w-[20px] cursor-pointer"
-      >
-        <path d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748Z"></path>
-      </svg>
+      <SubMenuBar />
     </div>
   );
 }
