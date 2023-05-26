3e63899a9f862eee0bf22d3e2e0318ed898a4db1
LeeSangCheol <leesangcheol@LeeSangCheols-MacBook-Air.local>
2023-05-26 20:14:36 +0900

    feat : sectionBox (정책 프리뷰) 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,46 @@
 import ImgSlider from "./etc/ImgSlider";
+import PolicyBoxAtHome from "./etc/PolicyBoxAtHome";
+import SectionBox from "./etc/SectionBox";
 
 export default function MainPage() {
-  return <ImgSlider />;
+  return (
+    <>
+      <ImgSlider />
+      <SectionBox
+        title={
+          <p className="text-[16px] font-semibold text-[#1C1C1E]">
+            곧 <span className="text-[#3592FF]">마감</span>되는 정책들이 있어요!
+          </p>
+        }
+        url={"/hello"}
+      >
+        <div className="flex overflow-auto space-x-[16px]">
+          <div className="w-[9px]" />
+          <PolicyBoxAtHome />
+          <PolicyBoxAtHome />
+          <PolicyBoxAtHome />
+          <PolicyBoxAtHome />
+          <PolicyBoxAtHome />
+        </div>
+      </SectionBox>
+      <SectionBox
+        title={
+          <p className="text-[16px] font-semibold text-[#1C1C1E]">
+            사람들이 <span className="text-[#3592FF]">주목</span>하고 있는
+            정책들이 있어요!
+          </p>
+        }
+        url={"/hello"}
+      >
+        <div className="flex overflow-auto space-x-[16px]">
+          <div className="w-[9px]" />
+          <PolicyBoxAtHome />
+          <PolicyBoxAtHome />
+          <PolicyBoxAtHome />
+          <PolicyBoxAtHome />
+          <PolicyBoxAtHome />
+        </div>
+      </SectionBox>
+    </>
+  );
 }
